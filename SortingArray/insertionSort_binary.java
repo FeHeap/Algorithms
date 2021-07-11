945cb1e34e8ed0cb9f301dfac66b00af70e3cca1
FeHeap <82155803+FeHeap@users.noreply.github.com>
2021-07-11 14:49:15 +0800

    Update insertionSort_binary.java

--- SortingArray/insertionSort_binary.java
+++ SortingArray/insertionSort_binary.java
@@ -1,14 +1,5 @@
 public class insertionSort_binary extends SortingArray{
 
-    public static void main(String[] args) {
-        SortingArray t = new heapSort();
-        int[] array = {0,-1,5,6,2,2,3,4,23,6,4,5,-1323,2,3256,1,-6561};
-        array = t.sorting(array);
-        for(int i = 0;i<array.length;i++){
-            System.out.print(array[i]+" ");
-        }
-    }
-
     public int[] sorting(int[] A_toSort){
         int[] ArrayBuf = new int[A_toSort.length];
         System.arraycopy(A_toSort, 0, ArrayBuf, 0, A_toSort.length);
